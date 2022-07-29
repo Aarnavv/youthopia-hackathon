15d325ce93bbb9ba0308e7ac587c70eee744656b
Aarnavv <aarnavagarwal.2016@gmail.com>
2022-07-29 22:35:56 +0530

    backend fix

--- src/components/Announcements.js
+++ src/components/Announcements.js
@@ -3,52 +3,55 @@ import "./css/announcements.css";
 
 
 class Announcements extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-			announcements : []
-		}
+      announcements: []
+    }
   }
-	componentDidMount() {
+  componentDidMount() {
     fetch('http://localhost:8000/announcements/')
-			.then(async (res) => await res.json())
-			.then((announcements) =>
-				this.setState((state) => {
-					return state.announcements.push(announcements)
-				})
-			)
-	}
-	setAnnouncements(){
-		return (
-			console.log(this.state.announcements[0].map((el) => {
-							return (
-								<div className="one-announcement">
-								<div className="bold"> By {el.title}, </div>
-								<div className="bold"> {el} </div>
-								<br />
-								<br />
-								<div className="message">
-								{
-									'{ANNOUNCEMENT: This is to notify that the electricity supply will be cut off from 12:00 hours to 16:00 hours, on 31st July, 2022, due to renovation work.}'
-								}
-								</div>
-								<br />
-								</div>
-								)
-								})
-					)
-		)
-	}
-	render() {
-		return (
-				<div className="announcements">
-				<div className="announcements-heading">
-					<h1 className="announcements-title">Announcements</h1>
-				</div>
-				{this.setAnnouncements()}
-				</div>
-		)
-	}
+      .then(async (res) => await res.json())
+      .then((announcements) =>
+        this.setState((state) => {
+          return state.announcements.push(announcements)
+        })
+      )
+  }
+  setAnnouncements() {
+    return (
+      this.state.announcements.map((el) => {
+        console.log(el)
+        return (
+          el.map((announcement) => {
+            return (
+              <div className="one-announcement">
+                <div className="bold"> By {announcement.fields.title}, </div>
+                <div className="bold"> {announcement.fields.description} </div>
+                <br />
+                <br />
+                <div className="message">
+                  {announcement.fields.description}
+                </div>
+                <br />
+              </div>
+            )
+          })
+
+        )
+      })
+    )
+  }
+  render() {
+    return (
+      <div className="announcements">
+        <div className="announcements-heading">
+          <h1 className="announcements-title">Announcements</h1>
+        </div>
+        {this.setAnnouncements()}
+      </div>
+    )
+  }
 }
 
 
